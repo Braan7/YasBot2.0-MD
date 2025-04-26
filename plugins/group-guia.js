ffe72dcd01217f74acbde8059a1c5747ff1b4fca
Braan7 <braanx7@gmail.com>
2025-04-26 16:11:09 -0600

    Update group-guia.js

--- plugins/group-guia.js
+++ plugins/group-guia.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn }) => {
             throw `❗ 𝐈𝐍𝐅𝐎 ❗ 𝚈𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚂𝚃𝙴 𝚃𝚄 𝙲𝙾𝙵𝚁𝙴\n𝚅𝚄𝙴𝙻𝚅𝙴 𝙴𝙽 *${remainingTime}* 𝙿𝙰𝚁𝙰 𝚅𝙾𝙻𝚅𝙴𝚁 𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚁`;
         }
 
-        const img = 'https://i.postimg.cc/Vv73j0HY/IMG-6032.jpg';
+        const img = '';
         const texto = `
         🤖 *Guía básica de comandos:*
 
