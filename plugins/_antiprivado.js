89c441cb2b95b18629b339d35a8e8e3ca0386f97
Braan7 <braanx7@gmail.com>
2025-04-27 06:09:32 -0500

    Actualizar _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -6,7 +6,9 @@ export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) 
   const chat = global.db.data.chats[m.chat];
   const bot = global.db.data.settings[this.user.jid] || {};
   if (bot.antiPrivate && !isOwner && !isROwner) {
-    await m.reply(`> "⭐ Hola @${m.sender.split`@`[0]}, Lo Siento No Esta 📌Permitido Escribirme Al Privado ⚠️ Por Lo Cual Seras Bloqueado/A\n\n> *⭐ Wiii \n\n\n Venta de plataformas, bots, etc.\n Contacto del creador:\n+52 998 140 3699`, false, {mentions: [m.sender]});
+    await m.reply(`> "⭐ Hola @${m.sender.split`@`[0]}, 𝙇𝙤 𝙨𝙞𝙚𝙣𝙩𝙤! 𝙉𝙤 𝙚𝙨𝙩á 𝙥𝙚𝙧𝙢𝙞𝙩𝙞𝙙𝙤 𝙚𝙨𝙘𝙧𝙞𝙗𝙞𝙧𝙢𝙚 𝙥𝙤𝙧 𝙥𝙧𝙞𝙫𝙖𝙙𝙤.
+
+𝐕𝐞𝐧𝐭𝐚 𝐝𝐞 𝐩𝐥𝐚𝐭𝐚𝐟𝐨𝐫𝐦𝐚𝐬 𝐬𝐭𝐫𝐞𝐚𝐦𝐢𝐧𝐠 𝐲 𝐛𝐨𝐭 𝐩𝐚𝐫𝐚 𝐠𝐫𝐮𝐩𝐨𝐬:\n\n> *⭐ Wiii \n\n\n Venta de plataformas, bots, etc.\n Contacto del creador:\+52 442 572 0856`, false, {mentions: [m.sender]});
     await this.updateBlockStatus(m.chat, 'block');
   }
   return !1;
